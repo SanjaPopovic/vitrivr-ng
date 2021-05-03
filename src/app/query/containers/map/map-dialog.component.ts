17484a102ea45cc6b0f6965a11fa73a46c1791b3
SanjaPopovic <sanja.popovic@stud.unibas.ch>
2021-05-03 22:46:59 +0200

    Add toolbar to draw circle, rectangle and polyline

--- src/app/query/containers/map/map-dialog.component.ts
+++ src/app/query/containers/map/map-dialog.component.ts
@@ -1,10 +1,7 @@
 import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
-import {MapQueryTerm} from '../../../shared/model/queries/map/map-query-term.model';
 import * as L from 'leaflet';
-import {SemanticMap} from '../../../shared/model/queries/semantic/semantic-map.model';
 import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
-import {SemanticSketchDialogComponent} from '../semantic/semantic-sketch-dialog.component';
-import {first} from 'rxjs/operators';
+import 'leaflet-draw/dist/leaflet.draw';
 
 @Component({
   selector: 'app-qt-map-dialog',
@@ -27,6 +24,33 @@ export class MapDialogComponent implements OnInit {
     });
 
     tiles.addTo(this.popUpMap);
+
+    const drawnItems = new L.FeatureGroup();
+    this.popUpMap.addLayer(drawnItems);
+
+    const drawControl = new L.Control.Draw({
+      draw: {
+        polyline: true,
+        polygon: false,
+        circlemarker: false,
+        rectangle: true,
+        marker: false,
+        edit: false
+      },
+      edit: {
+        featureGroup: drawnItems, // tell which layer is editable
+        edit: false
+      }
+    });
+    this.popUpMap.addControl(drawControl);
+
+    this.popUpMap.on('draw:created', function (e) {
+      const type = e.layerType,
+        layer = e.layer;
+      if (type === 'rectangle') {layer.bindPopup('A rectangle!'); }
+      drawnItems.addLayer(layer);
+    });
+
   }
 
   constructor(private _dialog: MatDialog, private _dialogRef: MatDialogRef<MapDialogComponent>) { }
